1a7362942fcf1d57caa009086a5a8bd582be4903
Muhammad Ramzan <jani.ramzan@hotmail.com>
2019-09-11 23:49:54 +0100

    Created Layered Identity project with multiple db contexts including db first for database and code first for Identity.

---
+++ IdentityCore/IdentityCore.Api/Entities/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace IdentityCore.Api.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ IdentityLayered/IdentityLayered.IdentityModels/Entities/ApplicationDbContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace IdentityLayered.IdentityModels.Entities
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public ApplicationDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //DO NOT DELETE. THIS IS USED TO ADD MIGRATIONS AND UPDATE DATABASE LOCALLY
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+                //DO NOT DELETE, BELOW LINE IS REQUIRED WHEN WORKING WITH AZURE ENVIRONMENT CONFIGURATIONS
+                //string connectionString = Environment.GetEnvironmentVariable("ConnectionStrings:DefaultConnection").ToString();
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        //public DbSet<Tester> Testers { get; set; }
+    }
+}
